488c0452b6d8f9cc8e7f631ad9a36ebac40aaeae
Tahe <taha.bensaid132002@gmail.com>
2023-02-23 14:15:23 +0100

    first commit

---
+++ HeartShape/HeartShapeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeartShapeApp.swift
+//  HeartShape
+//
+//  Created by tahe on 20/2/2023.
+//
+
+import SwiftUI
+
+@main
+struct HeartShapeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HeartShapeView()
+        }
+    }
+}
